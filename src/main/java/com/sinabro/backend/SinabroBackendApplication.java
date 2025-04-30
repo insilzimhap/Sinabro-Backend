a0983c3b7974d1451cfc7ddeae14577facfe3399
mchae123 <moomcy611@gmail.com>
2025-05-01 00:44:49 +0900

    메인 브랜치에 기본 프로젝트 올림: 2025-05-01 (AM 12:44)

---
+++ src/main/java/com/sinabro/backend/SinabroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sinabro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinabroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinabroBackendApplication.class, args);
+	}
+
+}
