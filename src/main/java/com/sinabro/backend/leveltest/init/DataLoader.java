e4e44091c14011ac8d6faf32d49a277b7b240036
SJHawa <163496255+SJHawa@users.noreply.github.com>
2025-11-04 03:45:39 +0900

    -Fix: 레벨테스트 옵션 수정

--- src/main/java/com/sinabro/backend/leveltest/init/DataLoader.java
+++ src/main/java/com/sinabro/backend/leveltest/init/DataLoader.java
@@ -141,9 +141,9 @@ public class DataLoader implements CommandLineRunner {
         l2q1.setType("글자 고르기");
         l2q1.setPrompt("아래 보기 중 같은 글자를 골라보세요.");
         l2q1.setQuestionImageUrl("/img/question_ga.png");
-        l2q1.addOption(new LevelTestOption("나", "/img/ra.png", false));
+        l2q1.addOption(new LevelTestOption("라", "/img/ra.png", false));
         l2q1.addOption(new LevelTestOption("가", "/img/ga.png", true));
-        l2q1.addOption(new LevelTestOption("다", "/img/ma.png", false));
+        l2q1.addOption(new LevelTestOption("마", "/img/ma.png", false));
 
         LevelTestQuestion l2q2 = new LevelTestQuestion();
         l2q2.setLevel(2);
@@ -172,8 +172,8 @@ public class DataLoader implements CommandLineRunner {
         l3q2.setType("빈칸 글자 고르기");
         l3q2.setPrompt("그림을 보고, 빠진 글자를 찾아 고르세요.");
         l3q2.setQuestionImageUrl("/img/shoes_blank.png");
-        l3q2.addOption(new LevelTestOption("산", "/img/so.png", false));
-        l3q2.addOption(new LevelTestOption("심", "/img/san.png", false));
+        l3q2.addOption(new LevelTestOption("소", "/img/so.png", false));
+        l3q2.addOption(new LevelTestOption("산", "/img/san.png", false));
         l3q2.addOption(new LevelTestOption("신", "/img/shin.png", true));
 
         questionRepository.saveAll(List.of(l3q1, l3q2));
